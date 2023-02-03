c34116c671e261109bf3c159d92f68230f78f496
Subhadeep Banerjee <subhadeep762@gmail.com>
2023-02-03 16:23:47 +0530

    Add Card in NewsItem

--- src/components/NewsItem.jsx
+++ src/components/NewsItem.jsx
@@ -2,6 +2,26 @@ import React, { Component } from "react";
 
 export default class NewsItem extends Component {
   render() {
-    return <div>NewsItem</div>;
+    let { title, description, newsUrl, imageUrl } = this.props;
+
+    return (
+      <div className="my-3">
+        <div className="card" style={{ width: "18rem" }}>
+          <img src={imageUrl} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">{title}...</h5>
+            <p className="card-text">{description}...</p>
+            <a
+              href={newsUrl}
+              target="_blank"
+              className="btn btn-sm btn-primary"
+              rel="noreferrer"
+            >
+              Read More
+            </a>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
