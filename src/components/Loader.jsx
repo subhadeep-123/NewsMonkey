549ad8f65c0aa476b19b458f599fb653dbb6040b
Subhadeep Banerjee <subhadeep762@gmail.com>
2023-02-06 01:13:15 +0530

    Make loader centralized

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -5,7 +5,14 @@ import loading from "../assets/loader.gif";
 export default class loader extends Component {
   render() {
     return (
-      <div className="text-center">
+      <div
+        className="text-center"
+        style={{
+          position: "fixed",
+          top: "50%",
+          left: "50%",
+        }}
+      >
         <img src={loading} alt="API Loader" />
       </div>
     );
