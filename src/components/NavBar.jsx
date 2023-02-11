fe4bba8c50934cd2e306a86e59e464e7208db5c5
Subhadeep Banerjee <subhadeep762@gmail.com>
2023-02-11 14:16:43 +0530

    Make NavBar sticky

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 export const NavBar = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav className="navbar navbar-expand-lg fixed-top navbar-dark bg-dark">
       <div className="container-fluid">
         <Link className="navbar-brand" to="/">
           NewsMonkey
