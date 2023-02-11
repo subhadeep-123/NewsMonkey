c21f166c40991dffa3dc20313fb93f21f0274539
Subhadeep Banerjee <subhadeep762@gmail.com>
2023-02-11 17:35:02 +0530

    Make App.js function based component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 import LoadingBar from "react-top-loading-bar";
 
@@ -6,124 +6,114 @@ import { NavBar } from "./components/NavBar";
 import { News } from "./components/News";
 import About from "./components/About";
 
-export default class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      progress: 0,
-    };
-  }
+export const App = () => {
+  const [progress, setProgress] = useState(0);
+  const [countryName, setCountryName] = useState("us");
 
-  setProgress = (progress) => {
-    this.setState({ progress: progress });
-  };
-
-  render() {
-    const router = createBrowserRouter([
-      {
-        path: "/",
-        element: (
-          <div>
-            <LoadingBar
-              color="#f11946"
-              height={2}
-              progress={this.state.progress}
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+        <div>
+          <LoadingBar color="#f11946" height={2} progress={progress} />
+          {<NavBar setCountryName={setCountryName} />}
+          <Outlet />
+        </div>
+      ),
+      children: [
+        {
+          path: "/",
+          element: (
+            <News
+              setProgress={setProgress}
+              pageSize={6}
+              country={countryName}
             />
-            {<NavBar />}
-            <Outlet />
-          </div>
-        ),
-        children: [
-          {
-            path: "/",
-            element: (
-              <News setProgress={this.setProgress} pageSize={6} country="in" />
-            ),
-          },
-          {
-            path: "/business",
-            element: (
-              <News
-                setProgress={this.setProgress}
-                pageSize={6}
-                key="business"
-                country="in"
-                category="business"
-              />
-            ),
-          },
-          {
-            path: "/entertainment",
-            element: (
-              <News
-                setProgress={this.setProgress}
-                pageSize={6}
-                key="entertainment"
-                country="in"
-                category="entertainment"
-              />
-            ),
-          },
-          {
-            path: "/health",
-            element: (
-              <News
-                setProgress={this.setProgress}
-                pageSize={6}
-                key="health"
-                country="in"
-                category="health"
-              />
-            ),
-          },
-          {
-            path: "/science",
-            element: (
-              <News
-                setProgress={this.setProgress}
-                pageSize={6}
-                key="science"
-                country="in"
-                category="science"
-              />
-            ),
-          },
-          {
-            path: "/sports",
-            element: (
-              <News
-                setProgress={this.setProgress}
-                pageSize={6}
-                key="sports"
-                country="in"
-                category="sports"
-              />
-            ),
-          },
-          {
-            path: "/technology",
-            element: (
-              <News
-                setProgress={this.setProgress}
-                pageSize={6}
-                key="technology"
-                country="in"
-                category="technology"
-              />
-            ),
-          },
-          {
-            path: "/about",
-            element: <About />,
-          },
-        ],
-      },
-    ]);
+          ),
+        },
+        {
+          path: "/business",
+          element: (
+            <News
+              setProgress={setProgress}
+              pageSize={6}
+              key="business"
+              country={countryName}
+              category="business"
+            />
+          ),
+        },
+        {
+          path: "/entertainment",
+          element: (
+            <News
+              setProgress={setProgress}
+              pageSize={6}
+              key="entertainment"
+              country={countryName}
+              category="entertainment"
+            />
+          ),
+        },
+        {
+          path: "/health",
+          element: (
+            <News
+              setProgress={setProgress}
+              pageSize={6}
+              key="health"
+              country={countryName}
+              category="health"
+            />
+          ),
+        },
+        {
+          path: "/science",
+          element: (
+            <News
+              setProgress={setProgress}
+              pageSize={6}
+              key="science"
+              country={countryName}
+              category="science"
+            />
+          ),
+        },
+        {
+          path: "/sports",
+          element: (
+            <News
+              setProgress={setProgress}
+              pageSize={6}
+              key="sports"
+              country={countryName}
+              category="sports"
+            />
+          ),
+        },
+        {
+          path: "/technology",
+          element: (
+            <News
+              setProgress={setProgress}
+              pageSize={6}
+              key="technology"
+              country={countryName}
+              category="technology"
+            />
+          ),
+        },
+        {
+          path: "/about",
+          element: <About />,
+        },
+      ],
+    },
+  ]);
 
-    return (
-      <div>
-        <RouterProvider router={router} />
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
+};
