c61e01e2eb83066ede12c4ece3cbc3ee175c0ba4
Subhadeep Banerjee <subhadeep762@gmail.com>
2023-02-11 12:28:28 +0530

    Convert navbar to function based component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 import LoadingBar from "react-top-loading-bar";
 
-import NavBar from "./components/NavBar";
+import { NavBar } from "./components/NavBar";
 import News from "./components/News";
 import About from "./components/About";
 
